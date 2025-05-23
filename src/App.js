041df482c4bd5633cbcc7f18f33fa16960739cfd
Seojihee <official4016@nave.com>
2025-05-23 16:57:08 +0900

    feat: 카카오 로그인 code 감지 후 콜백 페이지로 자동 이동 처리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { HashRouter, Routes, Route, useNavigate } from "react-router-dom";
 
 import Header from "./components/Header";
 import "./styles/common.css";
 import SignupMethodPage from "./pages/SignupMethodPage";
 import KakaoCallbackPage from "./pages/KakaoCallbackPage";
 
-function App() {
+// 코드가 있는 경우 자동으로 콜백 페이지로 라우팅 처리하는 Wrapper
+const RedirectHandler = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    const code = url.searchParams.get("code");
+    if (code) {
+      navigate("/oauth/kakao/callback");
+    }
+  }, [navigate]);
+
   return (
     <>
       <Header />
@@ -15,6 +26,14 @@ function App() {
         <Route path="/oauth/kakao/callback" element={<KakaoCallbackPage />} />
       </Routes>
     </>
+  );
+};
+
+function App() {
+  return (
+    <HashRouter>
+      <RedirectHandler />
+    </HashRouter>
   );
 }
 
