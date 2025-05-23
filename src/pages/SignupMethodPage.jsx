fc573ad5abb310d252431eb979e9925b45a106f4
Seojihee <official4016@nave.com>
2025-05-23 16:41:21 +0900

    fix: REDIRECT_URI를 callback 라우트로 수정

--- src/pages/SignupMethodPage.jsx
+++ src/pages/SignupMethodPage.jsx
@@ -13,7 +13,8 @@ const SignupMethodPage = () => {
 
     const handleKakaoLogin = () => {
         const REST_API_KEY = "65295b9472b38097d11e3a81dd2b5983";
-        const REDIRECT_URI = "https://seojihee4016.github.io/uslog-home";
+        // const REDIRECT_URI = "https://seojihee4016.github.io/uslog-home";
+        const REDIRECT_URI = "https://seojihee4016.github.io/uslog-home/#/oauth/kakao/callback";
     
         const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
     
