bac5c96a8bb786fa7b0b35f7d4b5fcf9058622c0
Seojihee <official4016@nave.com>
2025-05-26 10:43:57 +0900

    fix: Router 중복 제거

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { useEffect } from "react";
-import { HashRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom"; 
 
 import Header from "./components/Header";
 import "./styles/common.css";
 import SignupMethodPage from "./pages/SignupMethodPage";
 import KakaoCallbackPage from "./pages/KakaoCallbackPage";
 
-// 코드가 있는 경우 자동으로 콜백 페이지로 라우팅 처리하는 Wrapper
 const RedirectHandler = () => {
   const navigate = useNavigate();
 
@@ -30,11 +29,7 @@ const RedirectHandler = () => {
 };
 
 function App() {
-  return (
-    <HashRouter>
-      <RedirectHandler />
-    </HashRouter>
-  );
+  return <RedirectHandler />;
 }
 
 export default App;
