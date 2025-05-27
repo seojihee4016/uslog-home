5a378a23aa049b7ce51318a79ee4029729fe8893
Seojihee <official4016@nave.com>
2025-05-27 15:43:49 +0900

    fix: 사용하지 않는 변수 제거 및 a 태그 접근성 오류 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,14 +6,14 @@ const Header = () => {
     <header className="header">
         <div className="header-inner">
             <div className="header-left">USLOG</div>
-                <div className="header-right">
-                    <li>
-                        <a href="#">회원가입</a> 
-                    </li>
-                    <li>
-                        <a href="#">로그인</a> 
-                    </li>
-                </div>
+            {/* <div className="header-right">
+                <li>
+                    <a href="#">회원가입</a>
+                </li>
+                <li>
+                    <a href="#">로그인</a> 
+                </li>
+            </div> */}
         </div>
     </header>
     );
--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 
-const basename = process.env.NODE_ENV === "development" ? "/" : "/uslog-home";
+// const basename = process.env.NODE_ENV === "development" ? "/" : "/uslog-home";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
