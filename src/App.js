0cad7252b8cd7557fc26b37ecf42297f1737d23d
Seojihee <official4016@nave.com>
2025-05-27 11:43:27 +0900

    배포 대응: HashRouter 도입 및 GitHub Pages용 homepage 설정

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React, { useEffect } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom"; 
+import {
+  BrowserRouter,
+  HashRouter,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
 
 import Header from "./components/Header";
 import "./styles/common.css";
 import SignupMethodPage from "./pages/SignupMethodPage";
 import KakaoCallbackPage from "./pages/KakaoCallbackPage";
 
+// Router 환경 구분
+const Router =
+  process.env.NODE_ENV === "development" ? BrowserRouter : HashRouter;
+
+// 인가코드가 있으면 자동으로 콜백 페이지로 이동
 const RedirectHandler = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
     const url = new URL(window.location.href);
-    const code = url.searchParams.get("code");
+    const code =
+      url.searchParams.get("code") ||
+      new URLSearchParams(window.location.hash.split("?")[1])?.get("code");
+
     if (code) {
       navigate("/oauth/kakao/callback");
     }
@@ -29,7 +43,11 @@ const RedirectHandler = () => {
 };
 
 function App() {
-  return <RedirectHandler />;
+  return (
+    <Router>
+      <RedirectHandler />
+    </Router>
+  );
 }
 
 export default App;
